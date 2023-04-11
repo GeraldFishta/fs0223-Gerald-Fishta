28bf7e9a08b8448ddd322736db8ca545c419fc90
GeraldFishta <ge.fishta@gmail.com>
2023-04-11 15:46:25 +0200

    dede

---
+++ m1/s3/g1/homework/D6.js
@@ -0,0 +1,147 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Se sei in difficoltà puoi chiedere aiuto a un Teaching Assistant
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per farlo puoi utilizzare il terminale Bash, quello di VSCode o quello del tuo sistema operativo (se utilizzi macOS o Linux)
+*/
+
+/* ESERCIZIO 1
+ Scrivi una funzione di nome "area", che riceve due parametri (l1, l2) e calcola l'area del rettangolo associato..
+*/
+
+function area(l1, l2){
+    return l1 * l2
+}
+console.log(area(5,3))
+
+
+/* ESERCIZIO 2
+ Scrivi una funzione di nome "crazySum", che riceve due numeri interi come parametri.
+ La funzione deve ritornare la somma dei due parametri, ma se il valore dei due parametri è il medesimo deve invece tornare
+ la loro somma moltiplicata per tre.
+*/
+
+function crazySum( a, b ){
+    if(a === b){
+        return (a+b)*3
+    }else{
+        return a+b
+    }
+    
+}
+console.log(crazySum(5,5))
+console.log(crazySum(5,3))
+
+
+
+
+/* ESERCIZIO 3
+ Scrivi una funzione di nome "crazyDiff" che calcola la differenza assoluta tra un numero fornito come parametro e 19.
+ Deve inoltre tornare la differenza assoluta moltiplicata per tre qualora il numero fornito sia maggiore di 19.
+*/
+
+function crazyDiff( c, d = 19){
+    difference = Math.abs(c - d)
+    if (difference < 19){
+        return difference
+    }else{
+        return (difference*3)
+    }
+}
+
+console.log(crazyDiff(80, 19))
+console.log(crazyDiff(22, 19))
+
+/* ESERCIZIO 4
+ Scrivi una funzione di nome "boundary" che accetta un numero intero n come parametro, e ritorna true se n è compreso tra 20 e 100 (incluso) oppure
+ se n è uguale a 400.
+*/
+
+function boundary(n){
+    if(n >= 20 && n <= 100 || n == 400){
+        return true
+    }else{
+        return false
+    }
+}
+console.log(boundary(189))
+console.log(boundary(89))
+
+/* ESERCIZIO 5
+ Scrivi una funzione di nome "epify" che accetta una stringa come parametro.
+ La funzione deve aggiungere la parola "EPICODE" all'inizio della stringa fornita, ma se la stringa fornita comincia già con "EPICODE" allora deve
+ ritornare la stringa originale senza alterarla.
+*/
+
+function epify(str){
+    if(str.includes("EPICODE")){
+        return str
+    }else{
+        return str + "EPICODE"
+    } 
+}
+console.log(epify("deffeee"))
+console.log(epify("EPICODEbabybabyyy"))
+
+
+/* ESERCIZIO 6
+ Scrivi una funzione di nome "check3and7" che accetta un numero positivo come parametro. La funzione deve controllare che il parametro sia un multiplo
+ di 3 o di 7. (Suggerimento: usa l'operatore modulo)
+*/
+
+function check3and7(np){
+    if(np % 3 === 0 && np % 7 === 0){
+        return true
+    }else{
+        return false
+    }
+}
+
+/* ESERCIZIO 7
+ Scrivi una funzione di nome "reverseString", il cui scopo è invertire una stringa fornita come parametro (es. "EPICODE" --> "EDOCIPE")
+*/
+
+function reverseString(stringa){
+    return stringa.split("").reverse().join("")  //trasformo la stringa in array, lo divido nei suoi caratteri, e li riunisco con join dopo una funzione reverse.
+}
+console.log(reverseString("contrario"))
+
+/* ESERCIZIO 8
+ Scrivi una funzione di nome "upperFirst", che riceve come parametro una stringa formata da diverse parole.
+ La funzione deve rendere maiuscola la prima lettera di ogni parola contenuta nella stringa.
+*/
+
+
+function upperFirst(y){
+    
+    return y.charAt(0).toUpperCase() + y.slice()
+}
+console.log(upperFirst("quando andavo al lago me ne stavo") )  //rivedere, solo primo carattere della prima parola => dopo trasforma y in array e forEach...blabla
+
+/* ESERCIZIO 9
+ Scrivi una funzione di nome "cutString", che riceve come parametro una stringa. La funzione deve creare una nuova stringa senza il primo e l'ultimo carattere
+ della stringa originale.
+*/
+
+function cutString(str1){
+    return str1.slice(1,str1.length-1)
+}
+console.log(cutString("Volere"))
+
+
+
+/* ESERCIZIO 10
+ Scrivi una funzione di nome "giveMeRandom", che accetta come parametro un numero n e ritorna un'array contenente n numeri casuali inclusi tra 0 e 10.
+*/
+
+function giveMeRandom(n){
+    let arr = []                                   //creo array
+    for(let i = 0; i < n; i++){                     //ciclo 
+        arr.push(Math.floor(Math.random()*10))      //richiedo numeri casuali da pushare dentro arr  
+    }
+    return arr                                       //richiedo il risultato
+} 
+
+console.log(giveMeRandom(9))
