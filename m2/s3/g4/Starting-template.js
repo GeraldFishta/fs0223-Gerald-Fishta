8b830f330d89d1f44fbcede82692a36906628ce7
GeraldFishta <ge.fishta@gmail.com>
2023-05-11 16:49:49 +0200

    esercizio 3 fatto

--- m2/s3/g4/Starting-template.js
+++ m2/s3/g4/Starting-template.js
@@ -1,24 +1,74 @@
-let loadImagesButton = document.getElementById('load-images')
+
+let card = document.getElementsByClassName('card');
+let cardsArray = [...card];
+const loadImagesButton = document.getElementById('load-images');
+const loadSecondaryImagesButton = document.getElementById('load-secondary-images');
 
 loadImagesButton.addEventListener('click', () => {
-    utl 
-})
 
-
-
-
-
-
-
-
-
-
-/*fetch('https://example.com/api/data', {
+fetch('https://api.pexels.com/v1/search?query=Nature', {
   headers: {
-    'Authorization' : '[LJPG30hSUDtKBI5JESCA24GdRtKJ4X7wqPuCSY1hBPzFxDRVPT11IoWI]'
+    Authorization: 'LJPG30hSUDtKBI5JESCA24GdRtKJ4X7wqPuCSY1hBPzFxDRVPT11IoWI'
   }
 })
+.then(response => response.json())
+.then((photos) => {
+
+  console.log(photos.photos);
+
+  cardsArray.forEach((element, i) => {
+    console.log('sono dentro al for');
+     element.firstElementChild.remove() 
+     let newImg = document.createElement('img')
+     newImg.style.objectFit = 'cover';
+     newImg.height = "225"
+     newImg.src = photos.photos[i].src.landscape
+     cardsArray[i].prepend(newImg)
+    })
+    })
+    
+      .catch((error) => {
+      console.error(error);
+    });
+  })
+  
+loadSecondaryImagesButton.addEventListener('click', () => {
+
+  fetch('https://api.pexels.com/v1/search?query=People', {
+    headers: {
+      Authorization: 'LJPG30hSUDtKBI5JESCA24GdRtKJ4X7wqPuCSY1hBPzFxDRVPT11IoWI'
+    }
+  })
   .then(response => response.json())
-  .then(data => console.log(data))
-  .catch(error => 
-    console.error(error));  */
+  .then((photos) => {
+  
+    console.log(photos.photos);
+  
+    cardsArray.forEach((element, i) => {
+      console.log('sono dentro al for');
+       element.firstElementChild.remove() 
+       let newImg = document.createElement('img')
+       newImg.style.objectFit = 'cover';
+       newImg.height = "225"
+       newImg.src = photos.photos[i].src.landscape
+       cardsArray[i].prepend(newImg)
+      })
+      })
+      
+        .catch((error) => {
+        console.error(error);
+      });
+    })
+
+let buttonHide = document.querySelectorAll('.btn-group button:nth-child(2)')
+console.log(buttonHide)
+
+buttonHide.forEach((element) => {
+  element.innerHTML = 'Hide'
+  element.addEventListener('click', () =>{
+    element.closest('.col-md-4').remove()
+  })
+})
+
+
+
