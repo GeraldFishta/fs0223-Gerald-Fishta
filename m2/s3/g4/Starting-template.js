c7a43119591c40f7bf941f60bd533e344d57bcf7
GeraldFishta <ge.fishta@gmail.com>
2023-05-11 11:12:11 +0200

    da

---
+++ m2/s3/g4/Starting-template.js
@@ -0,0 +1,24 @@
+let loadImagesButton = document.getElementById('load-images')
+
+loadImagesButton.addEventListener('click', () => {
+    utl 
+})
+
+
+
+
+
+
+
+
+
+
+/*fetch('https://example.com/api/data', {
+  headers: {
+    'Authorization' : '[LJPG30hSUDtKBI5JESCA24GdRtKJ4X7wqPuCSY1hBPzFxDRVPT11IoWI]'
+  }
+})
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => 
+    console.error(error));  */
