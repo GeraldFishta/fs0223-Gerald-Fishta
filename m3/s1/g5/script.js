05677ddb9ebf6c67294caa9f2bdc22e676c98e4e
GeraldFishta <ge.fishta@gmail.com>
2023-05-27 16:10:54 +0200

    registro Chiamate finito

--- m3/s1/g5/script.js
+++ m3/s1/g5/script.js
@@ -1,19 +1,55 @@
 "use strict";
+/*
+Extra:
+
+Aggiungi una proprietà registroChiamate che contenga un array di oggetti contenenti i seguenti dati:
+id
+durata
+Data ed ora della chiamata
+
+Aggiorna la classe in modo che il registro funzioni, e crea i seguenti metodi:
+mostraRegistroChiamate() => mostra tutte le chiamate effettuate
+filtraChiamatePerDataOra() => mostra solo le chiamate effettuate in una determinata data ed ora
+*/
 class SmartPhone {
     ricarica($) { this.carica = $ + this.carica; }
     numero404() { return `il tuo credito e ${this.carica}`; }
     getNumeroChiamate() { return this.numeroChiamate; }
-    chiamata(min) { this.carica = this.carica - min * this.costoMinuto; }
-    azzeraChiamate() { this.chiamata(0); }
-    constructor(carica, numeroChiamate, costoMinuto) {
+    chiamata(min) {
+        this.carica = this.carica - min * this.costoMinuto;
+        this.numeroChiamate = this.numeroChiamate + 1;
+        let chiamata = {
+            id: this.registroChiamate.length + 1,
+            durata: min,
+            dataeOra: new Date()
+        };
+        this.registroChiamate.push(chiamata);
+    }
+    azzeraChiamate() { this.numeroChiamate = 0; }
+    mostraRegistroChiamate() {
+        this.registroChiamate.forEach((element) => {
+            console.log(element);
+            console.log(`Questa chiamata ha e la numero ${element.id}`);
+            console.log(`Questa chiamata ha ha una durata di ${element.durata}`);
+            console.log(`Questa chiamata e stata effetuata il ${element.dataeOra}`);
+        });
+    }
+    constructor(carica) {
         this.carica = carica;
-        this.numeroChiamate = numeroChiamate;
-        this.costoMinuto = costoMinuto;
+        this.numeroChiamate = 0;
+        this.costoMinuto = 0.20;
+        this.registroChiamate = [];
     }
 }
-let MioTelefono = new SmartPhone(5, 0, 0.20);
-console.log(MioTelefono.ricarica(5));
+let MioTelefono = new SmartPhone(5);
+MioTelefono.ricarica(5);
 console.log(MioTelefono.numero404());
-console.log(MioTelefono.chiamata(12));
+MioTelefono.chiamata(12);
+MioTelefono.ricarica(5);
+MioTelefono.ricarica(8);
+MioTelefono.chiamata(3);
+MioTelefono.chiamata(6);
 console.log(MioTelefono.numero404());
+console.log(MioTelefono.getNumeroChiamate());
+MioTelefono.mostraRegistroChiamate();
 console.log(MioTelefono.azzeraChiamate());
--- m3/s1/g5/script.ts
+++ m3/s1/g5/script.ts
@@ -1,3 +1,24 @@
+/*
+Extra:
+
+Aggiungi una proprietà registroChiamate che contenga un array di oggetti contenenti i seguenti dati:
+id
+durata
+Data ed ora della chiamata
+
+Aggiorna la classe in modo che il registro funzioni, e crea i seguenti metodi:
+mostraRegistroChiamate() => mostra tutte le chiamate effettuate
+filtraChiamatePerDataOra() => mostra solo le chiamate effettuate in una determinata data ed ora  
+*/
+
+
+
+interface IChiamata {
+    id:number;
+    durata:number;
+    dataeOra: Date;
+}
+
 interface ISmartPhone {
     carica:number;
     numeroChiamate:number;
@@ -16,24 +37,55 @@ class SmartPhone implements ISmartPhone {
     carica:number
     numeroChiamate:number;
     costoMinuto:number;
+    registroChiamate:IChiamata[] 
 
-    ricarica($:number):void | number {this.carica = $ + this.carica}
+
+    ricarica($:number):void {this.carica = $ + this.carica}
     numero404():string{return `il tuo credito e ${this.carica}`}
     getNumeroChiamate():number{return this.numeroChiamate}
-    chiamata(min:number):void{ this.carica = this.carica - min * this.costoMinuto}
-    azzeraChiamate():void{this.numeroChiamate = 0}
+    chiamata(min:number):void{ this.carica = this.carica - min * this.costoMinuto
 
-    constructor (carica:number, numeroChiamate:number, costoMinuto:number){
+        this.numeroChiamate = this.numeroChiamate + 1;
+
+        let chiamata:IChiamata = {
+            id: this.registroChiamate.length + 1,
+            durata: min,  
+            dataeOra: new Date() 
+        }
+
+    this.registroChiamate.push(chiamata)
+    }
+    azzeraChiamate():void{this.numeroChiamate = 0}
+    mostraRegistroChiamate():void{this.registroChiamate.forEach((element) =>{
+        console.log(element)
+        console.log(`Questa chiamata ha e la numero ${element.id}`)
+        console.log(`Questa chiamata ha ha una durata di ${element.durata}`)
+        console.log(`Questa chiamata e stata effetuata il ${element.dataeOra}`)
+    })
+    
+    }
+
+    constructor (carica:number){
             this.carica = carica;
-            this.numeroChiamate = numeroChiamate;
-            this.costoMinuto = costoMinuto
+            this.numeroChiamate = 0;
+            this.costoMinuto = 0.20;
+            this.registroChiamate = []
+
 }}
 
 
-let MioTelefono = new SmartPhone (5, 0, 0.20)
+let MioTelefono = new SmartPhone (5)
 
-console.log(MioTelefono.ricarica(5))
+MioTelefono.ricarica(5)
 console.log(MioTelefono.numero404())
-console.log(MioTelefono.chiamata(12))
+MioTelefono.chiamata(12)
+MioTelefono.ricarica(5)
+MioTelefono.ricarica(8)
+MioTelefono.chiamata(3)
+MioTelefono.chiamata(6)
 console.log(MioTelefono.numero404())
+console.log(MioTelefono.getNumeroChiamate())
+MioTelefono.mostraRegistroChiamate()
 console.log(MioTelefono.azzeraChiamate())
+
+
