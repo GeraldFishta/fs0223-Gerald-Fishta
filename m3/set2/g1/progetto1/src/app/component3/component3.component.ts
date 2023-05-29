d56a5631b4e583d3505f03a6dfc0e96e7d4ccd49
GeraldFishta <ge.fishta@gmail.com>
2023-05-29 15:18:31 +0200

    yo

---
+++ m3/set2/g1/progetto1/src/app/component3/component3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-component3',
+  templateUrl: './component3.component.html',
+  styleUrls: ['./component3.component.css']
+})
+export class Component3Component {
+  title = "Ciao Comp3!"
+  title1 = "Comp3 works!!"
+}
