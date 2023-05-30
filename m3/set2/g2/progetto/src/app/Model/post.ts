20b973343552b85f5958dd141763401bcc56a0dc
GeraldFishta <ge.fishta@gmail.com>
2023-05-30 16:14:15 +0200

    card dsiplay problems

---
+++ m3/set2/g2/progetto/src/app/Model/post.ts
@@ -0,0 +1,13 @@
+export class Post {
+
+  title:string
+  body:string
+  id:number
+
+  constructor(title:string, body:string, id:number) {
+    this.title = title;
+    this.body = body;
+    this.id = id;
+  }
+
+}
