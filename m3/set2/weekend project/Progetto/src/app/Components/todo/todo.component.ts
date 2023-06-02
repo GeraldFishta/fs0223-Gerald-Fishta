bebb7ba2fa6c827c5c8f827c4426beddc1653b66
GeraldFishta <ge.fishta@gmail.com>
2023-06-02 22:59:38 +0200

    changes

--- m3/set2/weekend project/Progetto/src/app/Components/todo/todo.component.ts
+++ m3/set2/weekend project/Progetto/src/app/Components/todo/todo.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ClassTodo } from 'src/app/Classes/class-todo';
+import { Todo } from 'src/app/Interfaces/todo';
+import { TodosService } from 'src/app/Services/todos.service';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
   styleUrls: ['./todo.component.scss']
 })
-export class TodoComponent {
 
-}
+
+export class TodoComponent implements OnInit {
+
+  isLoading = true
+  todos: Todo [] = []
+  newTask: Todo = new ClassTodo("", false )
+
+  constructor(private todosService: TodosService) {}
+
+      ngOnInit() : void {
+
+      this.todosService.getTodos().then ((todos) => {
+        this.todos = todos;
+
+      this.isLoading = false;
+
+      })
+
+    }
+
+    addNewTask() : void {
+      this.todosService.addTodo(this.newTask)
+    }
+
+  }
+
+
+
