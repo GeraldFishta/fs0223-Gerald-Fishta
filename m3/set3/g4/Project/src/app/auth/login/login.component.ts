ceb360105168f32c2b01f65da4bf3571c5c5f671
GeraldFishta <ge.fishta@gmail.com>
2023-06-08 16:09:35 +0200

    da sistemare login, register ok

--- m3/set3/g4/Project/src/app/auth/login/login.component.ts
+++ m3/set3/g4/Project/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../auth.service';
+import { Login } from 'src/app/interface/login';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+
+  constructor(
+    private authSvc: AuthService
+  ){}
+
+
+  data:Login = {
+    email: '',
+    password: ''
+  }
+
+  login(){
+    this.authSvc.login(this.data)
+    .subscribe(accessData => {
+      alert(`Sei loggato come ${accessData.user.name}`)
+    })
+  }
 
 }
--- m3/set3/g4/Project/src/app/auth/register/register.component.ts
+++ m3/set3/g4/Project/src/app/auth/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Register } from 'src/app/interface/register';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +8,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+
+
+  constructor(
+    private authSvc: AuthService
+  ){
+
+  }
+
+  data:Register = {
+    email: '',
+    password: '',
+    name: '',
+    surname: ''
+  };
+
+  register(){
+    this.authSvc.signUp(this.data)
+    .subscribe(accessData => {
+      alert(accessData.user.name)
+    })
+  }
 
 }
