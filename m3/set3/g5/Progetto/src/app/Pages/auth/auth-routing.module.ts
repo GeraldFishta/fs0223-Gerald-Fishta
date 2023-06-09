1f1cb9d57caf3b9c897867d8d6663df47efc90bf
GeraldFishta <ge.fishta@gmail.com>
2023-06-09 16:06:32 +0200

    crude finita

--- m3/set3/g5/Progetto/src/app/Pages/auth/auth-routing.module.ts
+++ m3/set3/g5/Progetto/src/app/Pages/auth/auth-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from '../dashboard/dashboard.component';
 
 const routes: Routes = [
   { path: '', component: AuthComponent },
   { path: 'login', component: LoginComponent },
-  { path: 'register', component: RegisterComponent }
+  { path: 'register', component: RegisterComponent },
+  { path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
